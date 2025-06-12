7447b135b74890e27ce44021dba32b55580cad58
Onesmus Mutyauvyu <89151741+Onesmus123@users.noreply.github.com>
2025-06-12 07:07:15 +0300

    Updated insertInto.js file

--- insertInto.js
+++ insertInto.js
@@ -16,14 +16,18 @@ connection.connect(error => {
     }else{
         console.log("Database connection is successful.");
 
-        const queryData = `INSERT INTO adminNames VALUES (5616, 'Evan Mutua', 'Ngei');`
+        const queryData = `
+        INSERT INTO adminNames (admin_id, first_name, last_name)
+        VALUES (5616, 'Evan Mutua', 'Ngei')
+        `
+
         connection.query(queryData, (error, result) => {
             if(error){
                 console.log("Unable to insert data into the table");
                 console.log(error.message);
             }else{
-                console.log("Data inserted successfully.");
-                console.table(result);
+                console.log("Successful. Number of inserted records: " + result.affectedRows);
+                console.table(result)
             }
             connection.end();
         });
